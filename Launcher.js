56cca47b9babd4781297c7bccce09056e1437d8f
satwikkurapati <67234649+satwikkurapati@users.noreply.github.com>
2020-08-28 14:24:40 -0400

    Add files via upload

---
+++ Launcher.js
@@ -0,0 +1,46 @@
+class Launcher{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.pointB = pointB
+        this.launcher = Constraint.create(options);
+        World.add(world, this.launcher);
+    }
+    attach(body){
+        this.launcher.bodyA = body;
+    }
+    
+    fly(){
+        this.launcher.bodyA = null;
+    }
+
+    display(){
+        if(this.launcher.bodyA){
+            var pointA = this.launcher.bodyA.position;
+            var pointB = this.pointB;
+            push();
+            
+            stroke(48,22,8);
+            if(pointA.x < 220) {
+                strokeWeight(7);
+                line(pointA.x - 20, pointA.y, pointB.x -10, pointB.y);
+                line(pointA.x - 20, pointA.y, pointB.x + 30, pointB.y - 3);
+                image(this.launcher,pointA.x -30, pointA.y -10,15,30);
+            }
+            else{
+                strokeWeight(3);
+                line(pointA.x + 25, pointA.y, pointB.x -10, pointB.y);
+                line(pointA.x + 25, pointA.y, pointB.x + 30, pointB.y - 3);
+                image(this.launcher,pointA.x + 25, pointA.y -10,15,30);
+            }
+           
+            
+            pop();
+        }
+    }
+    
+}
